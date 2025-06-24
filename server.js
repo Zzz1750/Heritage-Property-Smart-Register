065183cc037f6a2f5643a2f366ab476d1c5782f9
Zzz1750 <antonyzharon2@gmail.com>
2025-06-24 20:20:00 +0530

    Update Vercel 2

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
 
 // Log all incoming requests
 app.use((req, res, next) => {
