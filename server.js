1c163249b4fccd619e1e55c47d17278c5b42117b
Zzz1750 <antonyzharon2@gmail.com>
2025-06-24 21:12:56 +0530

    updated import

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const cors = require('cors');
+const path = require('path');
 
 // Set default environment variables if not provided
 if (!process.env.MONGODB_URI) {
